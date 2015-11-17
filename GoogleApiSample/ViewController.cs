aaa35aee30a4506eaf9104fbacac306002f000af
Dave Humphreys <davidhumphreys@MacBook-Pro.local>
2015-11-17 22:13:47 +0000

    Added mac sample
    Added mac sample

---
+++ GoogleApiSample/ViewController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace GoogleApiSample
+{
+	public partial class ViewController : NSViewController
+	{
+		//private GoogleAuthenticator mGoogleAuth;
+
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			//mGoogleAuth = new GoogleAuthenticator ();
+
+			// Do any additional setup after loading the view.
+		}
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+		partial void didClickAuthenticate (NSObject sender)
+		{
+			//mGoogleAuth.Authenticate();
+		}
+	}
+}
